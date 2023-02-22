201edfd3c304a79d0ea8b7e4454acaa2d5ff226c
Ricard Torres <ricard@ricard.dev>
2023-02-22 08:21:07 +0100

    .

--- src/js/config.js
+++ src/js/config.js
@@ -5,7 +5,7 @@ export const VALUES = {
 export const INITIAL_DATA = {
   exercises: [
     { id: 0, name: "Push ups", goal: 60, completion: 0, done: 0 },
-    { id: 1, name: "Squats", goal: 100, completion: 0, done: 0 }
+    { id: 1, name: "Squats", goal: 200, completion: 0, done: 0 }
   ],
   water: {
     goal: 5,
